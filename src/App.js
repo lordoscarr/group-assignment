c4d3a6d60c287081fd95627c2ed6dbb27ab571fc
Oscar Andersson <oscar.andersson3@ikea.com>
2019-05-20 11:11:24 +0200

    added voi api stuff

--- src/App.js
+++ src/App.js
@@ -27,6 +27,41 @@ export class MapContainer extends Component {
   };
 
   render() {
+    // let bikes = [];
+
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function () {
+    //     let coords = navigator.geolocation.coords;
+    //     console.log('lat: ' + coords.latitude + ', long: ' + coords.longitude);
+
+    //     // Set up our HTTP request
+    //     var xhr = new XMLHttpRequest();
+
+    //     // Setup our listener to process completed requests
+    //     xhr.onload = function () {
+
+    //       // Process our return data
+    //       if (xhr.status >= 200 && xhr.status < 300) {
+    //         console.log('success!', xhr);
+    //         bikes = xhr;
+    //         console.log(JSON.parse(bikes));
+    //       } else {
+    //         console.log('The request failed!');
+    //       }
+    //     };
+
+    //     // Create and send a GET request
+    //     // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
+    //     // The second argument is the endpoint URL
+    //     xhr.open('GET', "https://api.voiapp.io/v1/vehicle/status/ready?lat=" + coords.latitude + "&lng=" + coords.longitude);
+    //     xhr.send();
+    //   });
+    // } else {
+    //   // Browser doesn't support Geolocation
+    //   console.log('map error');
+    // }
+
+    console.log('location: ' + this.state.mapCenter);
     return (
       <CurrentLocation
         centerAroundCurrentLocation
